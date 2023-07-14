b47f9b7113e24d4e55057b9ec6aa9a400afec9d5
wangchao <zjsxwc@qq.com>
2023-07-14 14:48:57 +0800

    more verbose print

--- main.go
+++ main.go
@@ -17,16 +17,20 @@ func main() {
 			chineseWords += " "
 		}
 	}
+
+	fmt.Println("把汉字转换为数字标记声调的拼音，比如 ā 为 a1、á 为 a2、ǎ 为 a3、à 为 a4，只能处理非多音字，多音字需要手动修改。")
+	fmt.Println("比如 `hi,我是 中国人` 的结果是 `hi,wo3shi4 zhong1guo2ren2`。 ")
+
 	if len(chineseWords) == 0 {
-		chineseWords = "hi,我是_中国人"
-		fmt.Println("没有参数，使用例子`hi,我是_中国人`")
+		fmt.Println("没有参数，请输入，例子 `./ChineseToNumericPinyin hi,我是 中国人`")
 	}
 
 	str, err := pinyin.New(chineseWords).Split("").Mode(pinyin.ToneUseNumeric).Convert()
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
-		fmt.Println(str) //输出 hi,wo3shi4_zhong1guo2ren2
+		fmt.Print("`"+ chineseWords +"` 的转换结果是： ")
+		fmt.Println(str)
 	}
 
 }
